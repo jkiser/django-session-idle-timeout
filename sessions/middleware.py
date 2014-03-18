e4446cc6016c59f87349823868fb56bf5bb4f88f
John M. Kiser <jkiser@upenn.edu>
2014-03-18 13:26:59 -0400

    v. prod 18-Mar-2014

--- sessions/middleware.py
+++ sessions/middleware.py
@@ -1,6 +1,10 @@
 import datetime
 
-from django.http import HttpResponseRedirect
+from django.utils.translation import ugettext as _
+from django.contrib import messages
+from django.template.response import TemplateResponse
+from django.shortcuts import redirect
+from django.contrib.sites.models import get_current_site
 from django.conf import settings
 
 from accounts.utils import cosign_logout
@@ -9,6 +13,8 @@ from accounts.utils import cosign_logout
 class SessionIdleTimeout:
     """Middleware class to timeout a session after a specified time period.
     """
+    def __init__(self):
+        self.requested_path = ""
 
     def process_request(self, request):
         # Timeout is done only for authenticated logged in users.
@@ -16,17 +22,28 @@ class SessionIdleTimeout:
             current_datetime = datetime.datetime.now()
             # Timeout if idle time period is exceeded.
             if request.session.has_key('last_activity') and \
-                            (current_datetime - request.session['last_activity']).seconds > \
-                            settings.SESSION_IDLE_TIMEOUT:
+                (current_datetime - request.session['last_activity']).seconds > \
+                settings.SESSION_IDLE_TIMEOUT:
                 cosign_logout(request)
-                # Note, with redirect, the inbound request seems to not subsequently correspond
-                # to referer, but we assume that what the user actually wants to do is
-                # continue on to location that was interrupted by timing out
-                response = HttpResponseRedirect(settings.SESSION_TIMED_OUT_URL)
-                response.set_cookie('previous', value=request.path)
-                return response
+                messages.add_message(request, messages.ERROR, 'Your session has been timed out.')
+                self.requested_path = request.path
+                return redirect(settings.SESSION_TIMED_OUT_URL)
             # Set last activity time in current session.
             else:
                 request.session['last_activity'] = current_datetime
         return None
+
+    def process_template_response(self, request, response):
+        if request.path == settings.SESSION_TIMED_OUT_URL:
+            current_site = get_current_site(request)
+            current_app = "accounts"
+            template_name = "registration/timed_out.html"
+            context = {
+                "site": current_site,
+                "site_name": current_site.name,
+                "page_title": _("Session timeout"),
+                "previous_url": self.requested_path,
+                }
+            return TemplateResponse(request, template_name, context, current_app=current_app)
+        return response
 
